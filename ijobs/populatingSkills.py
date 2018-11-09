143d1e50c8987997d738e808349ebc63a282b9be
fabiomsq <43662435+fabiomsq@users.noreply.github.com>
2018-11-09 00:57:41 -0500

    added some changes in the left join code

--- ijobs/populatingSkills.py
+++ ijobs/populatingSkills.py
@@ -3,7 +3,9 @@ import numpy as np
 # from UoT_TermProject.ijobs.ds_excel import *
 
 skill_source = pd.read_excel('ds.xlsx', sheet_name='skill')
-df_skill_source = skill_source.astype('str', copy='false')
+# df_skill_source = skill_source.astype('str', copy='true')
+# df_skill_source['Skill_ID'] = df_skill_source['Skill_ID'].astype(int)
+
 
 df_kw_source = pd.read_excel('ds.xlsx', sheet_name='kw')
 # df_kw_source = kw_source.astype('str', copy='false')
@@ -49,10 +51,11 @@ for index, row in df_base.iterrows():
 
 df_tmp = df_base.merge(df_skill_aux, how='left', left_on='Job_ID', right_on='ID')
 
+df_tmp['Skill_ID'] = df_tmp['Skill_ID'].astype(int)
 
-jobs = pd.concat([df_tmp, df_skill_source], axis=1, join='inner')
+jobs = df_tmp.merge(skill_source, left_on='Skill_ID', right_on='Skill_ID', how='left')
 
-print(jobs)
+print(df_tmp)
 
 
 
